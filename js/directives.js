d44e84e03d51d395f61680518fa499775e7726a1
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-08 16:07:51 -0600

    Initial commit with custom directives.

---
+++ js/directives.js
@@ -0,0 +1,38 @@
+app.directive('teaInfo', ['meanTeaData', function(meanTeaData) {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/teaInfo.html',
+    controller: function($scope) {
+      $scope.teas = meanTeaData;
+    },
+  };
+}]);
+
+app.directive('indivTea', function() {
+  return {
+    scope: { tea: '=' },
+    templateUrl: 'partials/indivTea.html',
+  };
+});
+
+app.directive('teaImg', function() {
+  return {
+    scope: { tea: '=' },
+    templateUrl: 'partials/indivImg.html',
+  };
+});
+
+app.directive('navDir', function() {
+  return {
+    templateUrl: 'partials/nav.html'
+  };
+});
+
+app.directive('searchDir', ['getCategories', function(getCategories) {
+  return {
+    templateUrl: 'partials/search.html',
+    controller: function($scope) {
+      $scope.categories = getCategories.getEm();
+    }
+  };
+}]);
